80317e063989502f42cc170ef62cfed243e3e995
Antony Ng'ang'a <antony_mn@Antonys-MacBook-Pro.local>
2017-09-16 13:23:35 +0300

    linked bootstrap css files

--- src/main.js
+++ src/main.js
@@ -3,14 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// css imports
 import './assets/css/bootstrap.min.css'
+import './assets/css/carousel.css'
+import './assets/css/app.css'
+// css imports
+// import './assets/js/jquery.mixitup.min'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
   components: { App }
-})
+}).$mount('#app')
