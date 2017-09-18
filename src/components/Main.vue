50366b9ff26ae2188390c207dd48138975582538
Antony Ng'ang'a <antony_mn@Antonys-MacBook-Pro.local>
2017-09-18 17:35:28 +0300

    added gallery and about sections

---
+++ src/components/Main.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class="jumbotron rounded-0">
+      <!-- <div class="container about-intro rounded">
+        <h1 class="display-3">pixel photography</h1>
+        <!-- <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae et laborum hic est eum in voluptas! Blanditiis dignissimos earum molestiae officia numquam, rem excepturi quo laudantium. Doloremque nesciunt nemo deserunt!
+        </p> -->
+      <!-- </div> -->
+    </div>
+    <!-- END OF jumbotron -->
+
+    <!-- GALLERY NESTED ROW -->
+    <div class="container gallery">
+      <div class="row heading">
+        <p>the selected of</p>
+        <header>
+          <h1>portfolio</h1>
+        </header>
+      </div>
+      <div class="nested row">
+        <div class="box1">
+          <img class="card-img-top rounded" src="../assets/imgs/ gaze.jpg" alt="">
+        </div>
+        <div class="box2">
+          <img class="card-img-top rounded" src="../assets/imgs/see.jpg" alt="">
+        </div>
+        <div class="box3">
+          <img class="card-img-top rounded" src="../assets/imgs/third.jpg" alt="">
+        </div>
+        <div class="box4">
+          <img class="card-img-top rounded" src="../assets/imgs/third.jpg" alt="">
+        </div>
+        <div class="box5">
+          <img class="card-img-top rounded" src="../assets/imgs/boat.jpg" alt="">
+        </div>
+        <div class="box6">
+          <img class="card-img-top rounded" src="../assets/imgs/second.jpg" alt="">
+        </div>
+        <div class="box7">
+          <!-- <img class="card-img-top rounded" src="../assets/imgs/fourth.jpg" alt=""> -->
+        </div>
+        <div class="box8">
+          <img class="card-img-top rounded" src="../assets/imgs/kids.jpg" alt="">
+        </div>
+        <div class="box9">
+          <img class="card-img-top rounded" src="../assets/imgs/first.jpg" alt="">
+        </div>
+      </div>
+    </div>
+      <!-- .row -->
+
+      <div class="album About">
+        <div class="col-12">
+          <div class="row heading">
+            <p>something</p>
+            <header>
+              <h1>about me</h1>
+            </header>
+          </div>
+
+        </div>
+        <div class="d-none d-md-block">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit corporis odit minus eius reiciendis quas, in animi a nam, beatae similique alias. Illum, esse commodi corporis facere assumenda delectus consectetur.Modi tempore non, molestias aspernatur praesentium odit, voluptates blanditiis voluptatum ut minima vitae impedit voluptatibus, soluta saepe porro laudantium? Ea veritatis soluta eos inventore odit ut laboriosam mollitia placeat quidem?
+          </p>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main'
+}
+</script>
+
+
